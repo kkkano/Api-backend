a84d0514ab95badee6b64006fb78e92f734557d2
wx <657394554@qq.com>
2023-12-28 23:40:15 +0800

    实现用户调用成功次数加一

---
+++ src/main/java/com/yupi/project/mapper/UserInterfaceInfoMapper.java
@@ -0,0 +1,16 @@
+package com.yupi.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yupi.project.model.entity.UserInterfaceInfo;
+
+
+/**
+ * @Entity generator.domain.UserInterfaceInfo
+ */
+public interface UserInterfaceInfoMapper extends BaseMapper<UserInterfaceInfo> {
+
+}
+
+
+
+
---
+++ src/test/java/com/yupi/project/service/UserInterfaceInfoServiceTest.java
@@ -0,0 +1,23 @@
+package com.yupi.project.service;
+
+import org.junit.jupiter.api.Assertions;
+// 自动生成的包不对，要改成这个
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+@SpringBootTest
+public class UserInterfaceInfoServiceTest {
+
+    @Resource
+    private UserInterfaceInfoService userInterfaceInfoService;
+
+    @Test
+    public void invokeCount() {
+        // 调用了userInterfaceInfoService的invokeCount方法，并传入两个参数(1L, 1L)
+        boolean b = userInterfaceInfoService.invokeCount(1L, 1L);
+        // 表示断言b的值为true，即测试用例期望invokeCount方法返回true
+        Assertions.assertTrue(b);
+    }
+}
