62f4557c22e5eb199cfdc567cce2edb2aec35a6c
wx <657394554@qq.com>
2023-12-31 00:32:49 +0800

    请求转发 调用模拟接口

--- src/main/java/com/yupi/yuapigateway/YuapiGatewayApplication.java
+++ src/main/java/com/yupi/yuapigateway/YuapiGatewayApplication.java
@@ -12,21 +12,21 @@ public class YuapiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(YuapiGatewayApplication.class, args);
     }
-
-    // 这个注解用于创建一个 Spring Bean，即一个路由规则的构建器
-    @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        // 创建路由规则的构建器
-        return builder.routes()
-                // 定义路由规则，给该规则起一个名字 "tobaidu"
-                .route("tobaidu", r -> r.path("/baidu")
-                        // 将满足 "/baidu" 路径的请求转发到 "https://www.baidu.com"
-                        .uri("https://www.baidu.com"))
-                // 定义路由规则，给该规则起一个名字 "toyupiicu"
-                .route("toyupiicu", r -> r.path("/yupiicu")
-                        // 将满足 "/yupiicu" 路径的请求转发到 "http://yupi.icu"
-                        .uri("http://yupi.icu"))
-                // 创建并返回路由规则配置对象
-                .build();
-    }
+//
+//    // 这个注解用于创建一个 Spring Bean，即一个路由规则的构建器
+//    @Bean
+//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+//        // 创建路由规则的构建器
+//        return builder.routes()
+//                // 定义路由规则，给该规则起一个名字 "tobaidu"
+//                .route("tobaidu", r -> r.path("/baidu")
+//                        // 将满足 "/baidu" 路径的请求转发到 "https://www.baidu.com"
+//                        .uri("https://www.baidu.com"))
+//                // 定义路由规则，给该规则起一个名字 "toyupiicu"
+//                .route("toyupiicu", r -> r.path("/yupiicu")
+//                        // 将满足 "/yupiicu" 路径的请求转发到 "http://yupi.icu"
+//                        .uri("http://yupi.icu"))
+//                // 创建并返回路由规则配置对象
+//                .build();
+//    }
 }
