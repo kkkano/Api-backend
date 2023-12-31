d1a276fdc0ea6c7b6e056c097e15f55ae40e25be
wx <657394554@qq.com>
2023-12-31 21:29:44 +0800

    地址改成常量

--- yuapi-client-sdk/src/main/java/com/yupi/yuapiclientsdk/client/YuApiClient.java
+++ yuapi-client-sdk/src/main/java/com/yupi/yuapiclientsdk/client/YuApiClient.java
@@ -19,7 +19,7 @@ import static com.yupi.yuapiclientsdk.utils.SignUtils.genSign;
  * @author yupi
  */
 public class YuApiClient {
-
+    private static final  String GATEWAY_HOST="http://localhost:8090";
     private String accessKey;
 
     private String secretKey;
@@ -33,7 +33,7 @@ public class YuApiClient {
         // 可以单独传入http参数，这样参数会自动做URL编码，拼接在URL中
         HashMap<String, Object> paramMap = new HashMap<>();
         paramMap.put("name", name);
-        String result = HttpUtil.get("http://localhost:8123/api/name/", paramMap);
+        String result = HttpUtil.get(GATEWAY_HOST+"/api/name/", paramMap);
         System.out.println(result);
         return result;
     }
@@ -42,7 +42,7 @@ public class YuApiClient {
         // 可以单独传入http参数，这样参数会自动做URL编码，拼接在URL中
         HashMap<String, Object> paramMap = new HashMap<>();
         paramMap.put("name", name);
-        String result= HttpUtil.post("http://localhost:8123/api/name/", paramMap);
+        String result= HttpUtil.post(GATEWAY_HOST+"/api/name/", paramMap);
         System.out.println(result);
         return result;
     }
@@ -61,7 +61,7 @@ public class YuApiClient {
 
     public String getUserNameByPost(User user) {
         String json = JSONUtil.toJsonStr(user);
-        HttpResponse httpResponse = HttpRequest.post("http://localhost:8123/api/name/user")
+        HttpResponse httpResponse = HttpRequest.post(GATEWAY_HOST+"/api/name/user")
                 .addHeaders(getHeaderMap(json))
                 .body(json)
                 .execute();
